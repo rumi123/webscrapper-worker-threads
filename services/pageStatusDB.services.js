a27a83f56fe1f11d0afa97b14687c90788e7b7ef
rumi123 <ruminajeeb@gmail.com>
2025-02-16 12:39:54 +0530

    added try catch for db services

--- services/pageStatusDB.services.js
+++ services/pageStatusDB.services.js
@@ -3,20 +3,40 @@ import { PAGE_STATUS_ENUMS } from "../constants/pageStatus.Enum.js"
 import { PageStatus } from "../models/index.js"
 
 export const getAllPagesStatus = async () => {
-    return await PageStatus.findAll({ attributes: ['pageNumber', 'status', 'attempts', 'errorLog'], raw: true })
+    try {
+        return await PageStatus.findAll({ attributes: ['pageNumber', 'status', 'attempts', 'errorLog'], raw: true })
+    } catch (error) {
+        console.log('dbl', error);
+        throw error
+    }
 }
 
 export const getPageStatusByPageNumber = async (pageNumber) => {
-    return await PageStatus.findOne({ where: { pageNumber }, attributes: ['pageNumber', 'status', 'attempts', 'errorLog'], raw: true })
+    try {
+        return await PageStatus.findOne({ where: { pageNumber }, attributes: ['pageNumber', 'status', 'attempts', 'errorLog'], raw: true })
+    } catch (error) {
+        console.log('dbl', error);
+        throw error
+    }
 }
 
 export const getFailedPagesByPageNumberRange = async (range) => {
     const { start, end } = range
-    return await PageStatus.findAll({ where: { pageNumber: { [Op.between]: [start, end] }, status: PAGE_STATUS_ENUMS.FAILED, attempts: { [Op.lt]: 2 } }, raw: true })
+    try {
+        return await PageStatus.findAll({ where: { pageNumber: { [Op.between]: [start, end] }, status: PAGE_STATUS_ENUMS.FAILED, attempts: { [Op.lt]: 2 } }, raw: true })
+    } catch (error) {
+        console.log('dbl', error);
+        throw error
+    }
 }
 
 export const getPageStatusByStatus = async (status) => {
-    return await PageStatus.findAll({ where: { status }, attributes: ['pageNumber', 'status', 'errorLog', 'attempts'], raw: true })
+    try {
+        return await PageStatus.findAll({ where: { status }, attributes: ['pageNumber', 'status', 'errorLog', 'attempts'], raw: true })
+    } catch (error) {
+        console.log('dbl', error);
+        throw error
+    }
 }
 
 export const createPageStatus = async (data) => {
@@ -24,9 +44,15 @@ export const createPageStatus = async (data) => {
         return await PageStatus.create(data)
     } catch (error) {
         console.log('dbl', error);
+        throw error
     }
 }
 
 export const updatePageStatus = async (pageNumber, status, errorLog, attempts) => {
-    return await PageStatus.update({ status, errorLog, attempts }, { where: { pageNumber } })
+    try {
+        return await PageStatus.update({ status, errorLog, attempts }, { where: { pageNumber } })
+    } catch (error) {
+        console.log('dbl', error);
+        throw error
+    }
 }
